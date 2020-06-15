3cc2137bdbd7649893ebe15a715fccb3bbe4f96a
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2020-06-16 00:06:33 +0200

    bar added life Events

--- force-app/main/default/lwc/lifeEventItem/lifeEventItem.js
+++ force-app/main/default/lwc/lifeEventItem/lifeEventItem.js
@@ -2,6 +2,14 @@ import { LightningElement, api } from "lwc";
 
 export default class LifeEventItem extends LightningElement {
     @api item;
+    @api first;
+    @api last;
+
+    constructor() {
+        super();
+        this.first = false;
+        this.last = false;
+    }
 
     handleClick(event) {
         console.log(this.item.icon);
