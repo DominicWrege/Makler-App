843a276f265eb9f5d8f18d44b1c1b0c8d5f25a24
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2020-06-15 18:09:26 +0200

    cleanup

--- force-app/main/default/lwc/taskList/taskList.js
+++ force-app/main/default/lwc/taskList/taskList.js
@@ -14,21 +14,17 @@ export default class TaskList extends LightningElement {
 
     @wire(getTaskList)
     getTasks(zeug) {
-        // getTaskList().then(console.log);
         if (zeug.data) {
             this.tasks = zeug.data; //prop beschreiben
-            console.log(zeug.data);
         } else if (zeug.error) {
             console.error(zeug.error);
         }
-        // console.log(zeug.error);
     }
 
     handleCheck(event) {
-        // console.log(event.detail.status);
-        console.log("id: ", event.detail.id);
         const payload = { rid: event.detail.id, status: event.detail.status };
-        console.log(payload);
-        UpdateTask(payload).then(console.log).catch(console.error);
+        UpdateTask(payload)
+            .then((d) => console.log("update done!"))
+            .catch(console.error);
     }
 }
