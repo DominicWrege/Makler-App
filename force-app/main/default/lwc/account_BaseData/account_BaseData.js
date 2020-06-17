5c4fec361cccb41a967855afa861822aef10a341
charlottehaa <charlotte@c-haase.de>
2020-06-17 14:37:23 +0200

    Account_baseData dreck

--- force-app/main/default/lwc/account_BaseData/account_BaseData.js
+++ force-app/main/default/lwc/account_BaseData/account_BaseData.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 
-export default class Account_BaseData extends LightningElement {}
+import getBaseData from "@salesforce/apex/Account_BaseData.getBaseData"
+
+export default class Account_BaseData extends LightningElement {
+ 
+   
+     constructor() {
+        super(); 
+    }
+    
+    @wire(getBaseData)
+    getBaseData(dat){
+        if(dat.data){
+            this.accounts = dat.data; 
+        } else if(dat.error){
+            console.error(dat.error); 
+        }
+
+    }
+ 
+
+}
