b56e374fc1b5cec1abb7bcdee63ddb6dc8b95620
DomincWrege <dominic.wrege001@stud.fh-dortmund.de>
2020-06-24 18:56:13 +0200

    searchField Works

--- force-app/main/default/lwc/customLookup/customLookup.js
+++ force-app/main/default/lwc/customLookup/customLookup.js
@@ -1,16 +1,16 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class CustomLookup extends LightningElement {
-    @api childObjectApiName = 'Account'; //Contact is the default value
-    @api targetFieldApiName = 'Id'; //AccountId is the default value
-    @api fieldLabel = 'Your field label here';
+    @api childObjectApiName = "Account"; //Contact is the default value
+    @api targetFieldApiName = "Id"; //AccountId is the default value
+    @api fieldLabel = "Your field label here";
     @api disabled = false;
     @api value;
     @api required = false;
 
     handleChange(event) {
         // Creates the event
-        const selectedEvent = new CustomEvent('valueselected', {
+        const selectedEvent = new CustomEvent("valueselected", {
             detail: event.detail.value
         });
         //dispatching the custom event
@@ -19,7 +19,9 @@ export default class CustomLookup extends LightningElement {
 
     @api isValid() {
         if (this.required) {
-            this.template.querySelector('lightning-input-field').reportValidity();
+            this.template
+                .querySelector("lightning-input-field")
+                .reportValidity();
         }
     }
 }
--- force-app/main/default/lwc/searchField/searchField.js
+++ force-app/main/default/lwc/searchField/searchField.js
@@ -1,7 +1,10 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track, api } from "lwc";
+import searchForAccount from "@salesforce/apex/Searcher.searchForAccount";
 
 export default class SearchField extends LightningElement {
     initialized = false;
+    @track suggestions = [];
+    @api label = "Search";
 
     renderedCallback() {
         if (this.initialized) {
@@ -10,5 +13,39 @@ export default class SearchField extends LightningElement {
         this.initialized = true;
         let listId = this.template.querySelector("datalist").id;
         this.template.querySelector("input").setAttribute("list", listId);
+    }
+
+    handleSelection(event) {
+        event.preventDefault();
+        let selectedId = "";
+        for (let i of this.template.querySelectorAll(".valueList > option")) {
+            if (i.value === event.target.value) {
+                selectedId = i.id;
+                break;
+            }
+        }
+        const selectedEvent = new CustomEvent("selected", {
+            detail: {
+                id: selectedId
+            }
+        });
+        console.log(selectedEvent);
+        this.dispatchEvent(selectedEvent);
+    }
+    handleInput(event) {
+        const value = event.target.value;
+        this.fetchSuggestionsWithDelay(value);
+    }
+    fetchSuggestionsWithDelay(term) {
+        setTimeout(
+            () =>
+                searchForAccount({ term: term }).then((v) => {
+                    this.suggestions = v;
+                }),
+            300
+        );
+    }
+    debounce(funOnce, delay) {
+        setTimeout(funOnce, delay);
     }
 }
