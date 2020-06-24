b06321524106f1d3f4983d49ac704b6e6c62aab0
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2020-06-24 19:22:04 +0200

    fix searchfield

--- force-app/main/default/lwc/test_recordform/test_recordform.js
+++ force-app/main/default/lwc/test_recordform/test_recordform.js
@@ -1,12 +1,9 @@
 import { LightningElement, api, track, wire } from "lwc";
-import ACCOUNT_NAME_FIELD from "@salesforce/schema/Account.Name";
-import ACCOUNT_PHONE_FIELD from "@salesforce/schema/Account.Phone";
 
 export default class Test_recordform extends LightningElement {
     @api selectedRecordId;
 
     handleValueSelcted(event) {
-        console.log("id", event.detail.id);
         this.selectedRecordId = event.detail.id;
     }
 }
