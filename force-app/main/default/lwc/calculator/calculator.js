bb80cb2e24d7da280ffe5bcf12c7ab371c93ef34
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2020-07-24 23:51:13 +0200

    calculator with chartjs fianally :D

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -1,64 +1,61 @@
 import { LightningElement, track, wire } from "lwc";
-import { loadScript } from "lightning/platformResourceLoader";
-import Chartjs from "@salesforce/resourceUrl/Chartjs";
 import { fireEvent } from "c/pubsub";
 import { CurrentPageReference } from "lightning/navigation";
-
-const CHART_CONFIG = {
-    type: "pie",
-    data: {
-        datasets: [
-            {
-                data: [25, 100],
-                backgroundColor: ["#ff6384", "#ffcd56"]
-            }
-        ],
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: ["a", "b"],
-        options: {}
-    }
-};
+import { loadScript } from "lightning/platformResourceLoader";
+import Chartjs from "@salesforce/resourceUrl/Chartjs";
 
 class Expenses {
-    constructor(label, value) {
+    constructor(label, value, key) {
         this.label = label;
         this.value = value;
-        this.key = Math.random() * Math.random() * 1000;
+        if (key) {
+            this.key = key;
+        } else {
+            this.key = Expenses.genKey();
+        }
+    }
+    static genKey() {
+        return Math.floor(Math.random() * 100000 + 5, 2);
     }
 }
 
 export default class Calculator extends LightningElement {
-    chart = null;
     @wire(CurrentPageReference)
     pageRef;
     income = 0;
-    @track expensesElements = [new Expenses("Miete/Haus", 0)];
-    @track outNumber = 0;
+    rendered = false;
+    @track expensesElements = [new Expenses("Miete/Haus", 0, 1)];
+    @track verfuegbar = 0;
+    chart = null;
+    initChart() {
+        try {
+            Chart.defaults.global.defaultFontSize = 14;
+            const crtx = this.template.querySelector("canvas.chart");
 
-    // initChart() {
-    //     try {
-    //         // console.log(CHART_CONFIG);
-    //         // CHART_CONFIG.data.datasets[0].data[0] = this.einkommen;
-    //         // CHART_CONFIG.data.datasets[0].data[1] = this.ausgaben;
-    //         // const crtx = this.template
-    //         //     .querySelector("canvas.chart")
-    //         //     .getContext("2d");
-    //         // this.chart = new Chart(crtx, CHART_CONFIG);
-    //         // console.log(this.chart);
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // }
-    // async renderedCallback() {
-    //     // await loadScript(this, Chartjs);
-    //     // try {
-    //     //     this.initChart();
-    //     //     this.initChart();
-    //     // } catch (err) {
-    //     //     console.log(err);
-    //     // }
-    // }
-
+            if (window.innerWidth > 500) {
+                crtx.height = 180;
+            } else {
+                crtx.height = 280;
+            }
+            this.chart = new Chart(crtx, Calculator.getBar());
+        } catch (e) {
+            //console.log(e);
+        }
+    }
+    async renderedCallback() {
+        try {
+            if (!window.Chart) {
+                await loadScript(this, Chartjs);
+            }
+            if (!this.rendered) {
+                this.initChart();
+                this.initChart();
+                this.rendered = true;
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
     clearInput(e) {
         if (e.target.value == "0") {
             e.target.value = null;
@@ -70,35 +67,140 @@ export default class Calculator extends LightningElement {
         } else {
             this.income = 0;
         }
-
         fireEvent(this.pageRef, "incomeChanged", this.income);
-        this.calc();
+        this.calcValues();
     }
     addNewExpenseField() {
         const input = this.template.querySelector(
             "lightning-input.add-new-expense"
         );
         if (input.value && input.value > 0) {
-            this.expensesElements.push(
-                new Expenses("Weitere Ausgaben", input.value)
-            );
+            const number = parseInt(input.value, 10);
+            const expense = new Expenses("Weitere Ausgaben", input.value);
+            this.expensesElements.push(expense);
             input.value = 0;
+
+            this.verfuegbar = this.verfuegbar - number;
+            if (this.verfuegbar > 0) {
+                this.updateIncommingBar(this.verfuegbar);
+            } else {
+                this.updateIncommingBar(0);
+            }
+
+            this.unshiftDataSet(number, expense.key, "Ausgabe");
+            this.chart.update();
         }
     }
-    calc() {
-        let sumExpenses = 0;
-        for (const input of this.template.querySelectorAll(".expenses")) {
-            if (input.value !== "") {
-                sumExpenses += parseInt(input.value, 10);
+
+    unshiftDataSet(value, key, label) {
+        this.chart.data.datasets.unshift({
+            label: label,
+            backgroundColor: [Calculator.getRandomColor()],
+            data: [value],
+            key: key,
+            maxBarThickness: 120
+        });
+    }
+
+    updateExpenses(e) {
+        if (e && e.target.value != "" && e.target.name) {
+            const eventKey = e.target.name;
+            const newNumber = parseInt(e.target.value, 10);
+            for (let x of this.expensesElements) {
+                if (x.key == eventKey) {
+                    x.value = newNumber;
+                    break;
+                }
+            }
+
+            for (let i in this.chart.data.datasets) {
+                if (this.chart.data.datasets[i].key == eventKey) {
+                    this.chart.data.datasets[i].data[0] = newNumber;
+                    break;
+                }
             }
         }
-        this.outNumber = this.income - sumExpenses;
-
-        const outNumberElement = this.template.querySelector(".out-number");
-        if (this.outNumber >= 0) {
-            outNumberElement.style.color = "green";
-        } else {
-            outNumberElement.style.color = "red";
+        this.calcValues();
+    }
+    calcValues() {
+        let sumExpenses = 0;
+        for (let item of this.expensesElements) {
+            try {
+                sumExpenses += parseInt(item.value, 10);
+            } catch (eee) {}
         }
+        this.verfuegbar = this.income - sumExpenses;
+        if (this.verfuegbar > 0) {
+            this.updateIncommingBar(this.verfuegbar);
+        } else {
+            this.updateIncommingBar(0);
+        }
+        console.log("ss", this.verfuegbar);
+        this.chart.update();
+    }
+    updateIncommingBar(value) {
+        const end = this.chart.data.datasets.length - 1;
+        this.chart.data.datasets[end].data[0] = value;
+    }
+
+    static getBar() {
+        return {
+            type: "bar",
+            data: {
+                labels: [""],
+                datasets: [
+                    {
+                        key: 1,
+                        label: "Miete/Haus",
+                        maxBarThickness: 120,
+                        backgroundColor: ["coral"],
+                        data: [0]
+                    },
+                    {
+                        key: -11,
+                        label: "Nettoeinkommen",
+                        maxBarThickness: 120,
+                        backgroundColor: ["#5679C0"],
+                        data: [0]
+                    }
+                ]
+            },
+            options: {
+                tooltips: false,
+                title: {
+                    display: false,
+                    text: "Ausgaben"
+                },
+                responsive: true,
+                scales: {
+                    xAxes: [
+                        {
+                            stacked: true,
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }
+                    ],
+                    yAxes: [
+                        {
+                            ticks: {
+                                display: false,
+                                min: 0
+                            },
+                            stacked: true
+                        }
+                    ]
+                }
+            }
+        };
+    }
+
+    static getRandomColor() {
+        let letters = "0123456789ABCDEF";
+        let color = "#";
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
 }
--- force-app/main/default/lwc/versorgungsluecke/versorgungsluecke.js
+++ force-app/main/default/lwc/versorgungsluecke/versorgungsluecke.js
@@ -9,10 +9,10 @@ export default class Versorgungsluecke extends LightningElement {
     @track rente = 0;
     @track wunschRente = 0;
     @track moreIncoming = 0;
-    @track difference = 0;
+    @track versorgungsluecke = 0;
     year = 1;
     @track possibleSaving = 0;
-    @track outNumber = 0;
+    @track versorgung = 0;
     connectedCallback() {
         registerListener("incomeChanged", this.handleChange, this);
     }
@@ -25,11 +25,11 @@ export default class Versorgungsluecke extends LightningElement {
         } else {
             this.possibleSaving = 0;
         }
-        this.calcOut();
+        this.calcVersorgung();
     }
     yearChanged(e) {
         this.year = parseInt(e.target.value, 10);
-        this.calcOut();
+        this.calcVersorgung();
     }
     wunschRenteChanged(e) {
         if (e.target.value !== "") {
@@ -37,7 +37,7 @@ export default class Versorgungsluecke extends LightningElement {
         } else {
             this.wunschRente = 0;
         }
-        this.calcDifference();
+        this.calcVersoungsluecke();
     }
 
     moreIncomingChanged(e) {
@@ -46,39 +46,45 @@ export default class Versorgungsluecke extends LightningElement {
         } else {
             this.moreIncoming = 0;
         }
-        this.calcDifference();
+        this.calcVersoungsluecke();
     }
     clearInput(e) {
         if (e.target.value == "0") {
             e.target.value = null;
         }
     }
-    calcDifference() {
-        this.difference = this.wunschRente - this.moreIncoming - this.rente;
-        this.calcOut();
+    calcVersoungsluecke() {
+        this.versorgungsluecke =
+            this.wunschRente - this.moreIncoming - this.rente;
+        this.calcVersorgung();
     }
+
+    colorNumber(selector, value) {
+        const element = this.template.querySelector(selector);
+        if (value > 0) {
+            element.style = "green";
+        } else if (value < 0) {
+            element.style = "red";
+        } else {
+            element.style = "black";
+        }
+    }
+
     caclRente() {
         this.rente = this.incoming * 0.3781;
-        this.calcDifference();
+        this.calcVersoungsluecke();
     }
     handleChange(val) {
         this.incoming = val;
         this.caclRente();
-        this.calcDifference();
+        this.calcVersoungsluecke();
     }
-    calcOut() {
-        const v1 = 1.0237;
-        const v2 = 0.0237;
-        this.outNumber =
-            this.difference +
-            (this.possibleSaving + v1 * (Math.pow(v1, this.year - 1) / v2));
-
-        const outNumberElement = this.template.querySelector(".out-number");
-
-        if (this.outNumber >= 0) {
-            outNumberElement.style.color = "green";
-        } else {
-            outNumberElement.style.color = "red";
-        }
+    calcVersorgung() {
+        const x =
+            this.possibleSaving *
+            1.0237 *
+            (Math.pow(1.0237, this.year) - 1 / 0.0237);
+        this.versorgung = this.versorgungsluecke - x;
+        this.colorNumber(".versorgung", this.versorgung);
     }
 }
